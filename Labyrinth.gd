a32cc93fabf3f4c15f7b23bff9b013af2cb661dc
henrikcs <henrik.csoere@protonmail.com>
2021-05-29 11:45:34 +0200

    refactor

--- Labyrinth.gd
+++ Labyrinth.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-
-const GRID_HEIGHT = 25
-const GRID_WIDTH = 25
+# only kind of works with odd numbers
+const LABYRINTH_HEIGHT = 21
+# only kind of works with odd numbers
+const LABYRINTH_WIDTH = 21
 
 # Temp fix for placing walls
 const WALL_TILE = Vector2(0, 0)
@@ -13,64 +14,75 @@ const PASSAGE = 1
 
 var pendingCells: Array
 
-# lab (labyrinth) is a two dimensional array containing infos on which tile is 
+# labyrinth (labyrinth) is a two dimensional array containing infos on which tile is 
 # placed where.
-var lab = []
+var labyrinth = []
 
 func _ready():
-	lab.resize(GRID_HEIGHT)
+	randomize()
+	initLabyrinth()
 	
-	# init the labyrinth:
-	for i in GRID_HEIGHT:
-		lab[i] = []
-		lab[i].resize(GRID_WIDTH)
-		for j in GRID_WIDTH:
-			# init the Labyrinth:
-			lab[i][j] = WALL
-			$TileMap.set_cell(i, j, 0, false, false, false, WALL_TILE)
+	# Start from 1, 1
+	generateAndDrawLabyrinth(Vector2(1, 1))
+	
+	# now we have a quadrant ready for player 1 in the labyrinth list
+	# mirror it, then we get the maze for player 2
 	
 	
-	var randomCell = Vector2(randi() % GRID_HEIGHT, randi() % GRID_WIDTH)
-	lab[randomCell.x][randomCell.y] = PASSAGE
-	
-	$TileMap.set_cell(randomCell.x, randomCell.y, 0, false, false, false, PASSAGE_TILE)
-	
-	pendingCells = getNeighbouringCellsInState(randomCell, WALL, 1)
 
-func _process(delta):
-	OS.delay_msec(50)
-	if !pendingCells.empty():
+# using Randomized Prim's Algorithm to generate a maze
+func generateAndDrawLabyrinth(startingCell: Vector2):
+	labyrinth[startingCell.x][startingCell.y] = PASSAGE
+	drawTile(startingCell, PASSAGE_TILE)
+	
+	pendingCells = getNeighbouringCellsInState(startingCell, WALL, 1)
+
+	while !pendingCells.empty():
 		var randomWallIndex = randi() % pendingCells.size()
 		var randomWall = pendingCells[randomWallIndex]
 		
-		var neighboursOfRandomWall = getNeighbouringCellsInState(randomWall, PASSAGE, 1)
+		var neighboursOfRandomWall = getNeighbouringCellsInState(randomWall, PASSAGE)
 		
 		if neighboursOfRandomWall.size() != 1:
 			pendingCells.remove(randomWallIndex)
-			return
+			continue
 		
 		var dir = randomWall - neighboursOfRandomWall[0]
 		
 		var possibleCell = Vector2(dir.x + randomWall.x, dir.y + randomWall.y)
 		
-		if checkBounds(possibleCell) && lab[possibleCell.x][possibleCell.y] == WALL:
-			lab[randomWall.x][randomWall.y] = PASSAGE
-			lab[possibleCell.x][possibleCell.y] = PASSAGE
+		if checkBounds(possibleCell) && labyrinth[possibleCell.x][possibleCell.y] == WALL:
+			labyrinth[randomWall.x][randomWall.y] = PASSAGE
+			labyrinth[possibleCell.x][possibleCell.y] = PASSAGE
 			
-			$TileMap.set_cell(randomWall.x, randomWall.y, 0, false, false, false, PASSAGE_TILE)
-			$TileMap.set_cell(possibleCell.x, possibleCell.y, 0, false, false, false, PASSAGE_TILE)
+			drawTile(randomWall, PASSAGE_TILE)
+			drawTile(possibleCell, PASSAGE_TILE)
 			
-			# pendingCells.append_array(getNeighbouringCellsInState(randomWall, WALL, 1))
-			pendingCells.append_array(getNeighbouringCellsInState(possibleCell, WALL, 1))
+			pendingCells.append_array(getNeighbouringCellsInState(possibleCell, WALL))
 			
 		pendingCells.remove(randomWallIndex)
 		
+	# add starting Room and end room in the labyrinth
+	drawStartingRoom()
+	drawEndRoom()
 
-# Returns list of Vectors, which represent coordinates in lab.
-# the returned list of vectors is a list of frontier cells.
-# A frontier cell of a Cell is a cell with distance 2 in state Blocked and within the grid.
-func getNeighbouringCellsInState(cell: Vector2, state: int = WALL, distance: int = 2):
-	var frontierCells = []
+# inits the quadrant of the whole labyrinth with wall tiles
+func initLabyrinth():
+	labyrinth.resize(LABYRINTH_HEIGHT)
+	# init the labyrinth:
+	for i in LABYRINTH_HEIGHT:
+		labyrinth[i] = []
+		labyrinth[i].resize(LABYRINTH_WIDTH)
+		for j in LABYRINTH_WIDTH:
+			# init the Labyrinth:
+			labyrinth[i][j] = WALL
+			drawTile(Vector2(i, j), WALL_TILE)
+
+
+# Returns list of Vectors, which represent coordinates in labyrinth.
+# the returned list of vectors is a list of neighbouring cells.
+func getNeighbouringCellsInState(cell: Vector2, state: int = WALL, distance: int = 1):
+	var neighbourCells = []
 	
 	var up = cell.y - distance
 	var down = cell.y + distance
@@ -78,20 +90,40 @@ func getNeighbouringCellsInState(cell: Vector2, state: int = WALL, distance: int
 	var right = cell.x + distance
 	
 	# up
-	if up > 1 && lab[cell.x][up] == state:
-		frontierCells.append(Vector2(cell.x, up))
+	if up >= 0 && labyrinth[cell.x][up] == state:
+		neighbourCells.append(Vector2(cell.x, up))
 	# down
-	if down < GRID_HEIGHT && lab[cell.x][down] == state:
-		frontierCells.append(Vector2(cell.x, down))
+	if down < LABYRINTH_HEIGHT && labyrinth[cell.x][down] == state:
+		neighbourCells.append(Vector2(cell.x, down))
 	# left
-	if left > 1 && lab[left][cell.y] == state:
-		frontierCells.append(Vector2(left, cell.y))
+	if left >= 0 && labyrinth[left][cell.y] == state:
+		neighbourCells.append(Vector2(left, cell.y))
 	# right
-	if right < GRID_WIDTH && lab[right][cell.y] == state:
-		frontierCells.append(Vector2(right, cell.y))
+	if right < LABYRINTH_WIDTH && labyrinth[right][cell.y] == state:
+		neighbourCells.append(Vector2(right, cell.y))
 	
-	
-	return frontierCells
+	return neighbourCells
 
 func checkBounds(cell: Vector2):
-	return cell.x > 0 && cell.x < GRID_WIDTH &&  cell.y > 0 && cell.y < GRID_HEIGHT
+	return cell.x > 0 && cell.x < LABYRINTH_WIDTH &&  cell.y > 0 && cell.y < LABYRINTH_HEIGHT
+	
+func drawStartingRoom():
+	var startingPositions = [Vector2(0, 0), Vector2(0, 1), Vector2(1, 0)]
+	for pos in startingPositions:
+		drawTile(pos, PASSAGE_TILE)
+		labyrinth[pos.x][pos.y] = PASSAGE
+	
+func drawEndRoom():
+	var endingPositions = [Vector2(LABYRINTH_HEIGHT - 2, LABYRINTH_WIDTH - 1),
+		Vector2(LABYRINTH_HEIGHT - 1, LABYRINTH_WIDTH - 1),
+		Vector2(LABYRINTH_HEIGHT - 1, LABYRINTH_WIDTH - 2)]
+		
+	for pos in endingPositions:
+		drawTile(pos, PASSAGE_TILE)
+		labyrinth[pos.x][pos.y] = PASSAGE
+	
+# position: position in the tilemap
+# tile: position in the tileset
+func drawTile(position: Vector2, tile: Vector2):
+	$TileMap.set_cell(position.x, position.y, 0, false, false, false, tile)
+
