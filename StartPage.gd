150f957e1cad3e4aa08319f63ff802d93adbac23
Joniras <jonas.schaltegger@gmail.com>
2021-05-29 10:41:23 +0200

    Adapted Styles

--- StartPage.gd
+++ StartPage.gd
@@ -9,11 +9,20 @@ var game = preload("res://SplitScreen/Map.tscn").instance()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
 	get_node("VBoxContainer/Panel2/HBoxContainer2/optionsNumOfPlayers").grab_focus()
 
-
-func start_game(numberPlayers):
+func _on_joy_connection_changed(device_id, connected):
+	connected = [false, false, false, false]
+	for item in Input.get_connected_joypads():
+		connected[item] = true
 	
+	for item in connected:
+		# show color selection
+		pass
+		
+	
+func start_game(numberPlayers):
 	game.setupPlayer(numberPlayers)
 	get_node("/root").add_child(game)
 	get_node("/root").remove_child(self)
--- optionsNumOfPlayers.gd
+++ optionsNumOfPlayers.gd
@@ -8,9 +8,9 @@ extends OptionButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_item("2 players", 2);
-	add_item("3 players", 3);
-	add_item("4 players", 4);
+	add_item("2 players", 2)
+	add_item("3 players", 3)
+	add_item("4 players", 4)
 
 
 	
