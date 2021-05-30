86ca778d90682f2c4a2c0d880dc3c35aa84020ff
Joniras <jonas.schaltegger@gmail.com>
2021-05-30 14:26:13 +0200

    Item Fix

--- items/Item.gd
+++ items/Item.gd
@@ -12,7 +12,7 @@ onready var sprite = $Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite.texture = load("res://items/"+type+"_item.png")
+	sprite.texture = load("res://items/"+type+"_item_a.png")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
